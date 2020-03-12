ff8aa459a59a9fc22b9f57af1bf3463c78ac5014
javapark <javaparknet@gmail.com>
2020-03-12 18:42:29 +0900

    2 files modified

--- components/PhoneInfoList.js
+++ components/PhoneInfoList.js
@@ -8,7 +8,13 @@ class PhoneInfoList extends Component {
     onUpdate: () => console.warn('onUpdate not defined')
   }
 
+  // data가 현재 data 랑 다른 배열일 때 true 로 설정
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.data !== this.props.data;
+  }
+
   render(){
+    console.log('render PhoneInfoList');
     const {data, onRemove, onUpdate} = this.props;
     const list = data.map(
       info => (<PhoneInfo key={info.id} info={info} 
--- index.js
+++ index.js
@@ -1,57 +1,73 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import PhoneForm from './components/PhoneForm';
-import PhoneInfoList from './components/PhoneInfoList';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PhoneForm from "./components/PhoneForm";
+import PhoneInfoList from "./components/PhoneInfoList";
+import "./style.css";
 
 class App extends Component {
-  id = 2
+  id = 2;
   state = {
     information: [
       {
         id: 0,
-        name: '김민준',
-        phone: '010-0000-0000'
+        name: "김민준",
+        phone: "010-0000-0000"
       },
       {
         id: 1,
-        name: '홍길동',
-        phone: '010-0000-0001'
+        name: "홍길동",
+        phone: "010-0000-0001"
       }
-    ]
+    ],
+    keyword :''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    })
   }
 
   handleUpdate = (id, data) => {
-    const {information} = this.state;
+    const { information } = this.state;
     this.setState({
-      information: information.map(info => info.id === id ? {...info, ...data} : info)
-    })
-  }
+      information: information.map(info =>
+        info.id === id ? { ...info, ...data } : info
+      )
+    });
+  };
 
-  handleRemove = (id) => {
+  handleRemove = id => {
     const { information } = this.state;
     this.setState({
       information: information.filter(info => info.id != id)
-    })
-  }
+    });
+  };
 
-  handleCreate = (data)=>{
+  handleCreate = data => {
     const { information } = this.state;
     this.setState({
-      information : information.concat({ id: this.id++, ...data })
-    })
-  }
+      information: information.concat({ id: this.id++, ...data })
+    });
+  };
 
   render() {
+    const {information, keyword } = this.state;
     return (
       <div>
-        <PhoneForm onCreate={this.handleCreate}/>
-        <PhoneInfoList data={this.state.information} onRemove={this.handleRemove}
-        onUpdate={this.handleUpdate}
+        <PhoneForm onCreate={this.handleCreate} />
+        <p>
+          <input placeholder="검색 할 이름을 입력하세요.."
+          onChange={this.handleChange} value={keyword}/>
+        </p>
+        <PhoneInfoList
+          data={information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
         />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
