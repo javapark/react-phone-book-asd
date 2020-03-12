380560e9cc62555b4475227a579532cb23e87977
javapark <javaparknet@gmail.com>
2020-03-12 17:48:54 +0900

    1 file modified

--- index.js
+++ index.js
@@ -4,21 +4,37 @@ import PhoneForm from './components/PhoneForm';
 import './style.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
   }
 
   handleCreate = (data)=>{
-    console.log(data);
+    const { information } = this.state;
+    this.state = {
+      information : information.concat({ id: this.id++, ...data })
+    }
+
+    console.log(this.state);
   }
 
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate}/>
+        {JSON.stringify(information)}
       </div>
     );
   }
